9cb59b633aa683f3cf47d5b85cf0314a523a9cce
Gregor Vostrak <gregor@solidtime.io>
2024-08-27 23:40:49 +0200

    disable request and response validation for zod responses

--- src/main/autoUpdater.ts
+++ src/main/autoUpdater.ts
@@ -29,10 +29,10 @@ export function registerAutoUpdateListeners(mainWindow: Electron.BrowserWindow) 
     })
 
     getAutoUpdater().addListener('update-downloaded', () => {
-      app.emit('before-quit');
-      setTimeout(() => {
-        getAutoUpdater().quitAndInstall()
-      }, 500);
+        app.emit('before-quit')
+        setTimeout(() => {
+            getAutoUpdater().quitAndInstall()
+        }, 500)
     })
 
     getAutoUpdater().addListener('error', (error) => {
--- src/renderer/src/utils/api.ts
+++ src/renderer/src/utils/api.ts
@@ -4,6 +4,7 @@ import { accessToken, endpoint } from './oauth'
 
 export const apiClient = computed(() => {
     return createApiClient(endpoint.value + '/api', {
+        validate: 'none',
         axiosConfig: {
             headers: {
                 Authorization: `Bearer ${accessToken.value}`,
