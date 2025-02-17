91fd141238d348916f3c2f12140294a51e83829e
Thorge Dammann <moin@thorge.dev>
2025-02-17 10:32:28 +0100

    feat: time in tray

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -2,6 +2,9 @@ import { app, BrowserWindow, ipcMain, Menu, nativeImage, Tray } from 'electron'
 import activeTrayIcon from '../../resources/solidtime_trayTemplate@4x.png?asset'
 import inactiveTrayIcon from '../../resources/solidtime_emptyTemplate@4x.png?asset'
 import { TimeEntry } from '@solidtime/api'
+import dayjs from 'dayjs'
+
+let timerInterval: NodeJS.Timeout | undefined = undefined
 
 function buildMenu(mainWindow: BrowserWindow, timeEntry: TimeEntry | null) {
     const isRunning = !!(timeEntry && timeEntry.start && timeEntry.start !== '')
@@ -48,9 +51,25 @@ export function initializeTray(mainWindow: Electron.BrowserWindow) {
     // Create tray icon
     const tray = new Tray(nativeImage.createFromPath(inactiveTrayIcon))
     tray.setToolTip('solidtime')
+    tray.setTitle('')
     tray.setContextMenu(buildMenu(mainWindow, null))
 
     return tray
+}
+
+function updateTimerInterval(timeEntry: TimeEntry, tray: Tray) {
+    if (timeEntry && timeEntry.start && timeEntry.start !== '') {
+        const duration = dayjs().diff(dayjs(timeEntry.start), 'second')
+        // duration formatted to HH:MM
+        const hours = Math.floor(duration / 3600)
+            .toString()
+            .padStart(2, '0')
+        const minutes = Math.floor((duration % 3600) / 60)
+            .toString()
+            .padStart(2, '0')
+        const formattedDuration = `${hours}:${minutes}`
+        tray.setTitle(formattedDuration)
+    }
 }
 
 export function registerTrayListeners(tray: Tray, mainWindow: BrowserWindow) {
@@ -60,10 +79,21 @@ export function registerTrayListeners(tray: Tray, mainWindow: BrowserWindow) {
             if (timeEntry && timeEntry.start && timeEntry.start !== '') {
                 tray.setImage(nativeImage.createFromPath(activeTrayIcon))
                 tray.setToolTip('solidtime - Timer is running')
+                if (timerInterval) {
+                    clearInterval(timerInterval)
+                    timerInterval = undefined
+                }
+                timerInterval = setInterval(() => updateTimerInterval(timeEntry, tray), 1000)
                 tray.setContextMenu(buildMenu(mainWindow, timeEntry))
+                tray.setTitle('')
             } else {
+                if (timerInterval) {
+                    clearInterval(timerInterval)
+                    timerInterval = undefined
+                }
                 tray.setImage(nativeImage.createFromPath(inactiveTrayIcon))
                 tray.setToolTip('solidtime - Timer is stopped')
+                tray.setTitle('')
                 tray.setContextMenu(buildMenu(mainWindow, timeEntry))
             }
         }
