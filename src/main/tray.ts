d1b14e94e91744ee9e36170e0157490b545fccc6
Sigurdur Gudbrandsson <siggi@snjall.is>
2025-07-15 09:47:20 +0000

    Added inverted tray icon based on if the desktop is in dark mode or not

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -1,7 +1,19 @@
-import { app, BrowserWindow, ipcMain, Menu, nativeImage, Tray } from 'electron'
+import { app, BrowserWindow, ipcMain, Menu, nativeImage, Tray, nativeTheme } from 'electron'
 import activeTrayIcon from '../../resources/solidtime_tray@4x.png?asset'
 import inactiveTrayIcon from '../../resources/solidtime_empty@4x.png?asset'
+import activeTrayIconInverted from '../../resources/solidtime_tray_inverted@4x.png?asset'
+import inactiveTrayIconInverted from '../../resources/solidtime_empty_inverted@4x.png?asset'
 import { TimeEntry } from '@solidtime/api'
+
+let currentTrayTimeEntry: TimeEntry | null = null
+
+function getIconPath(active: boolean) {
+    const isDarkMode = nativeTheme.shouldUseDarkColors
+    if (active) {
+        return isDarkMode ? activeTrayIconInverted : activeTrayIcon
+    }
+    return isDarkMode ? inactiveTrayIconInverted : inactiveTrayIcon
+}
 
 function buildMenu(mainWindow: BrowserWindow, timeEntry: TimeEntry | null) {
     const isRunning = !!(timeEntry && timeEntry.start && timeEntry.start !== '')
@@ -45,10 +57,19 @@ function buildMenu(mainWindow: BrowserWindow, timeEntry: TimeEntry | null) {
 }
 
 export function initializeTray(mainWindow: Electron.BrowserWindow) {
-    // Create tray icon
-    const tray = new Tray(nativeImage.createFromPath(inactiveTrayIcon))
+    const tray = new Tray(nativeImage.createFromPath(getIconPath(false)))
     tray.setToolTip('solidtime')
     tray.setContextMenu(buildMenu(mainWindow, null))
+
+    nativeTheme.on('updated', () => {
+        const isRunning = !!(
+            currentTrayTimeEntry &&
+            currentTrayTimeEntry.start &&
+            currentTrayTimeEntry.start !== ''
+        )
+        tray.setImage(nativeImage.createFromPath(getIconPath(isRunning)))
+    })
+
     return tray
 }
 
@@ -56,15 +77,11 @@ export function registerTrayListeners(tray: Tray, mainWindow: BrowserWindow) {
     ipcMain.on('updateTrayState', (_, serializedTimeEntry: string) => {
         if (serializedTimeEntry) {
             const timeEntry = JSON.parse(serializedTimeEntry) as TimeEntry
-            if (timeEntry && timeEntry.start && timeEntry.start !== '') {
-                tray.setImage(nativeImage.createFromPath(activeTrayIcon))
-                tray.setToolTip('solidtime - Timer is running')
-                tray.setContextMenu(buildMenu(mainWindow, timeEntry))
-            } else {
-                tray.setImage(nativeImage.createFromPath(inactiveTrayIcon))
-                tray.setToolTip('solidtime - Timer is stopped')
-                tray.setContextMenu(buildMenu(mainWindow, timeEntry))
-            }
+            currentTrayTimeEntry = timeEntry
+            const isRunning = !!(timeEntry && timeEntry.start && timeEntry.start !== '')
+            tray.setImage(nativeImage.createFromPath(getIconPath(isRunning)))
+            tray.setToolTip(isRunning ? 'solidtime - Timer is running' : 'solidtime - Timer is stopped')
+            tray.setContextMenu(buildMenu(mainWindow, timeEntry))
         }
     })
 }
