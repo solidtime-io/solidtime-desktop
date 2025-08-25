f63e2b4848567a725ee568692088640a4df91606
Gregor Vostrak <gregor@solidtime.io>
2025-08-25 13:49:19 +0200

    fix token refresh logic

--- src/renderer/src/utils/api.ts
+++ src/renderer/src/utils/api.ts
@@ -1,14 +1,59 @@
 import { createApiClient } from '@solidtime/api'
 import { computed } from 'vue'
-import { accessToken, endpoint } from './oauth'
+import { accessToken, endpoint, refreshAccessToken, refreshToken } from './oauth'
+import {
+    type AxiosRequestConfig,
+    type AxiosResponse,
+    type AxiosError,
+    type InternalAxiosRequestConfig,
+} from 'axios'
 
 export const apiClient = computed(() => {
-    return createApiClient(endpoint.value + '/api', {
+    const client = createApiClient(endpoint.value + '/api', {
         validate: 'none',
-        axiosConfig: {
-            headers: {
-                Authorization: `Bearer ${accessToken.value}`,
-            },
-        },
     })
+
+    const axiosInstance = client.axios
+
+    axiosInstance.interceptors.request.use(
+        (config: InternalAxiosRequestConfig) => {
+            if (accessToken.value) {
+                config.headers.Authorization = `Bearer ${accessToken.value}`
+            }
+            return config
+        },
+        (error: AxiosError) => Promise.reject(error)
+    )
+
+    axiosInstance.interceptors.response.use(
+        (response: AxiosResponse) => response,
+        async (error: AxiosError) => {
+            const originalRequest = error.config as AxiosRequestConfig & { _retry?: boolean }
+
+            if (
+                error.response?.status === 401 &&
+                originalRequest &&
+                !originalRequest._retry &&
+                refreshToken.value
+            ) {
+                originalRequest._retry = true
+
+                try {
+                    await refreshAccessToken()
+                    // Ensure Authorization header is set for the retried request
+                    if (!originalRequest.headers) {
+                        originalRequest.headers = {}
+                    }
+                    originalRequest.headers.Authorization = `Bearer ${accessToken.value}`
+                    return axiosInstance.request(originalRequest)
+                } catch (refreshError) {
+                    return Promise.reject(refreshError)
+                }
+            }
+
+            return Promise.reject(error)
+        }
+    )
+
+    return client
 })
