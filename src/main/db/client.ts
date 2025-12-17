1b1ccd06cb7dced39cd90e0267a137db37a376e5
Gregor Vostrak <gregor@solidtime.io>
2025-12-17 22:07:40 +0100

    migrate to better-sqlite3

--- src/main/db/client.ts
+++ src/main/db/client.ts
@@ -1,5 +1,5 @@
-import { drizzle } from 'drizzle-orm/libsql'
-import { createClient } from '@libsql/client'
+import { drizzle } from 'drizzle-orm/better-sqlite3'
+import Database from 'better-sqlite3'
 import { app } from 'electron'
 import path from 'path'
 import * as schema from './schema'
@@ -7,10 +7,8 @@ import * as schema from './schema'
 // Create the database in the user data directory
 const dbPath = path.join(app.getPath('userData'), 'solidtime.db')
 
-// Create libsql client using file:// protocol for local SQLite
-const client = createClient({
-    url: `file:${dbPath}`,
-})
+// Create better-sqlite3 client
+const client = new Database(dbPath)
 
 export const db = drizzle(client, { schema })
 
