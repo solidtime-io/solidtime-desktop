e60b12483f5bb13b69e8e4467ee438d19375f02f
Gregor Vostrak <gregor@solidtime.io>
2026-03-30 00:00:20 +0200

    add window activities db indexes

--- drizzle/0000_panoramic_catseye.sql
+++ drizzle/0000_panoramic_catseye.sql
@@ -33,3 +33,7 @@ CREATE TABLE `window_activities` (
 	`process_id` integer,
 	`created_at` text NOT NULL
 );
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `idx_activity_periods_start_end` ON `activity_periods` (`start`, `end`);
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `idx_window_activities_timestamp` ON `window_activities` (`timestamp`);
